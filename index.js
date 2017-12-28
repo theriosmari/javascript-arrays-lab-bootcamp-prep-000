5a7eb3e48a9f8af333072fae10cb2e85319534ce
Mariel Ríos <marielbrios09@gmail.com>
2017-12-28 22:01:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const app = "I don't do much."
 var kittens = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten(name) {
-  destructivelyAppendKitten.push(name)
-  return destructivelyAppendKitten;
+  kittens.push(name)
+  return kittens;
 }
